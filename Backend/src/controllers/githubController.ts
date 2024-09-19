fd090b9268328a04bf8e26ef57f8d34e189ecb9c
cuneiform-akankshapatel <akanksha.patel@thecuneiform.com>
2024-09-19 10:38:09 +0530

    common api changes

--- Backend/src/controllers/githubController.ts
+++ Backend/src/controllers/githubController.ts
@@ -1,22 +1,65 @@
-import axios from 'axios';
-import constants from '../../helpers/constants';
+import constants from "../../helpers/constants";
+import axiosGet from "../../helpers/commonApi";
 
-export const getRepoList = async (req, res, next) => {
-  try {
-    const response = await axios.get(`${process.env.GIT_URL}/users/${req.params.username}/repos`);
-    const data = response.data;
-    res.status(constants.statusCodes.success).json({ data, message: constants.messages.getRepoListSuccess });
-  } catch (error) {
-    next(error);
-  }
+export const getRepoList = async (req: any, res: any) => {
+    try {
+        axiosGet(`users/${req.params.username}/repos?sort=updated&direction=desc&per_page=10`)
+            .then((result: any) => {
+                const list = result.data.filter(repo => !repo.private);
+                res.status(constants.statusCodes.success).json({
+                    data: list,
+                    message: constants.messages.getRepoListSuccess,
+                });
+            })
+            .catch(error => {
+                const statusCode = error.response ? error.response.status : constants.statusCodes.serverError;
+                res.status(statusCode).json({
+                    status: false,
+                    error: error.response ? error.response.data : error.message,
+                });
+            });
+    } catch (error) {
+        res.status(constants.statusCodes.serverError).json({
+            status: false,
+            error: error,
+        });
+    }
 };
 
-export const getCommitList = async (req, res, next) => {
-  try {
-    const response = await axios.get(`${process.env.GIT_URL}/repos/${req.params.owner}/${req.params.reponame}/commits`);
-    const data = response.data.slice(0, req.query.per_page);
-    res.status(constants.statusCodes.success).json({ data, totalCount: response.data.length, message: constants.messages.getCommitListSuccess });
-  } catch (error) {
-    next(error);
-  }
+export const getCommitList = async (req, res) => {
+    try {
+        const perPage = parseInt(req.params.per_page, 10) || 10; 
+        const page = parseInt(req.query.page) || 1; 
+        const skip = (page - 1) * perPage; 
+
+        axiosGet(`repos/${req.params.owner}/${req.params.reponame}/commits`)
+            .then((response: any) => {
+                const totalCount = response.data.length; // Total number of commits
+                const paginatedCommits = response.data.slice(skip, skip + perPage); 
+
+                res.status(constants.statusCodes.success).json({
+                    data: paginatedCommits,
+                    totalCount: totalCount,
+                    message: constants.messages.getRepoListSuccess,
+                });
+            })
+            .catch(error => {
+                const statusCode = error.response ? error.response.status : constants.statusCodes.serverError;
+                res.status(statusCode).json({
+                    status: false,
+                    error: error.response ? error.response.data : error.message,
+                });
+            });
+    } catch (error) {
+        const statusCode = error.response
+            ? error.response.status
+            : constants.statusCodes.serverError;
+        res.status(statusCode).json({
+            status: false,
+            error: error.response ? error.response.data : error.message,
+        });
+    }
 };
+
+module.exports = { getRepoList, getCommitList };
+
